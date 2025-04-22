18f09b2f376d231eecb3dc8dcb96bcf862c52ea9
loic-eni <160845586+loic-eni@users.noreply.github.com>
2025-04-22 14:47:33 +0200

    refactor: use signals and computed to compute distance and consommation on voyages update (#3)

--- src/app/carbon-footprint/carbon-footprint.component.ts
+++ src/app/carbon-footprint/carbon-footprint.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { CarbonFootprintFormComponent } from '../carbon-footprint-form/carbon-footprint-form.component';
 import { CarbonFootprintResultComponent } from '../carbon-footprint-result/carbon-footprint-result.component';
 import { CommonModule } from '@angular/common';
@@ -10,31 +10,19 @@ import { CommonModule } from '@angular/common';
   styleUrl: './carbon-footprint.component.scss'
 })
 export class CarbonFootprintComponent {
-  distanceKm = 0;
-  consommationPour100Km  = 0
-  add100Km = ()=> this.distanceKm += 100;
-  voyages = [
+  voyages = signal([
     { distanceKm: 50, consommationPour100Km: 5 },
     { distanceKm: 150, consommationPour100Km: 6 },
     { distanceKm: 250, consommationPour100Km: 7 },
     { distanceKm: 350, consommationPour100Km: 8 },
     { distanceKm: 450, consommationPour100Km: 9 }
-];
+]);
+  distanceKm = computed(()=> this.voyages().reduce((acc, v)=> acc + v.distanceKm, 0));
+  consommationPour100Km  = computed(()=> this.voyages().reduce((acc, v)=> acc + v.consommationPour100Km * v.distanceKm, 0)/(this.distanceKm()))
+
   genererVoyage(){
     const distanceKm = Math.floor(Math.random() * 750)
     const consommationPour100Km = Math.floor(Math.random() * 20)
-    this.voyages.push({distanceKm, consommationPour100Km})
-
-    this.updateConsomationsEtDistances();
-  }
-
-  updateConsomationsEtDistances(){
-    this.distanceKm = this.voyages.reduce((acc, v)=> acc + v.distanceKm, 0);
-    this.consommationPour100Km = this.voyages.reduce((acc, v)=> acc + v.consommationPour100Km * v.distanceKm, 0)/(this.distanceKm);
-  }
-
-
-  ngOnInit(){
-    this.updateConsomationsEtDistances();
+    this.voyages.update(voyages => [...voyages, {distanceKm, consommationPour100Km}])
   }
 }
