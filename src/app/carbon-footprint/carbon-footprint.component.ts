f1b330edf3b698d44b34c909b3252b69721b7fc2
Loic Duvail <loic.duvail2022@campus-eni.fr>
2025-04-22 14:04:51 +0200

    feat: level 2

--- src/app/carbon-footprint/carbon-footprint.component.ts
+++ src/app/carbon-footprint/carbon-footprint.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, signal } from '@angular/core';
 import { CarbonFootprintFormComponent } from '../carbon-footprint-form/carbon-footprint-form.component';
 import { CarbonFootprintResultComponent } from '../carbon-footprint-result/carbon-footprint-result.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-carbon-footprint',
-  imports: [CarbonFootprintFormComponent, CarbonFootprintResultComponent],
+  imports: [CarbonFootprintFormComponent, CarbonFootprintResultComponent, CommonModule],
   templateUrl: './carbon-footprint.component.html',
   styleUrl: './carbon-footprint.component.scss'
 })
@@ -12,4 +13,11 @@ export class CarbonFootprintComponent {
   distanceKm = 0;
   consommationPour100Km  = 0
   add100Km = ()=> this.distanceKm += 100;
+  voyages = [
+    { distanceKm: 50, consommationPour100Km: 5 },
+    { distanceKm: 150, consommationPour100Km: 6 },
+    { distanceKm: 250, consommationPour100Km: 7 },
+    { distanceKm: 350, consommationPour100Km: 8 },
+    { distanceKm: 450, consommationPour100Km: 9 }
+]
 }
