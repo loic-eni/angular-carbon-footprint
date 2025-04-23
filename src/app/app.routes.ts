7e0cc556ca0d3389236d591364d181a6f863cd78
Loic Duvail <loic.duvail2022@campus-eni.fr>
2025-04-23 14:51:24 +0200

    feat (level 3): add guard to home and carbon-footprint pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,16 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { CarbonFootprintComponent } from './pages/carbon-footprint/carbon-footprint.component';
+import { loginGuard } from './login.guard';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'carbon-footprint', component: CarbonFootprintComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [loginGuard] },
+  {
+    path: 'carbon-footprint',
+    component: CarbonFootprintComponent,
+    canActivate: [loginGuard],
+  },
   { path: '', pathMatch: 'full', redirectTo: '/carbon-footprint' },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
 ];
---
+++ src/app/login.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserService } from './core/services/user/user.service';
+
+export const loginGuard: CanActivateFn = (_route, _state) => {
+  const userService = inject(UserService);
+  const router = inject(Router);
+
+  if (!!userService.getUsername()().length) {
+    return true;
+  }
+
+  router.navigate(['/home']);
+  return false;
+};
